dd8cff54c02c4f83f348036c94706296d48e844a
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-09-03 10:26:22 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+**This is a draft. Don't use in production**
+
+# kiwi-suite/command-bus-validation
+
+[![Build Status](https://travis-ci.org/kiwi-suite/command-bus-validation.svg?branch=master)](https://travis-ci.org/kiwi-suite/command-bus-validation)
+[![Coverage Status](https://coveralls.io/repos/github/kiwi-suite/command-bus-validation/badge.svg?branch=develop)](https://coveralls.io/github/kiwi-suite/command-bus-validation?branch=develop)
+[![Packagist](https://img.shields.io/packagist/v/kiwi-suite/command-bus-validation.svg)](https://packagist.org/packages/kiwi-suite/command-bus-validation)
+[![Packagist Pre Release](https://img.shields.io/packagist/vpre/kiwi-suite/command-bus-validation.svg)](https://packagist.org/packages/kiwi-suite/command-bus-validation)
+[![Packagist](https://img.shields.io/packagist/l/kiwi-suite/command-bus-validation.svg)](https://packagist.org/packages/kiwi-suite/command-bus-validation)
+
+## Installation
+
+The suggested installation method is via [composer](https://getcomposer.org/):
+
+```sh
+php composer.phar require kiwi-suite/command-bus-validation
+```
+
+## License
+
+kiwi-suite/command-bus is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT) - see the `LICENSE` file for details
---
+++ src/ValidationHandler.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * kiwi-suite/command-bus-validation (https://github.com/kiwi-suite/command-bus-validation)
+ *
+ * @package kiwi-suite/command-bus-validation
+ * @link https://github.com/kiwi-suite/command-bus-validation
+ * @copyright Copyright (c) 2010 - 2018 kiwi suite GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+namespace KiwiSuite\CommandBusValidation;
+
+use KiwiSuite\CommandBus\Result\Result;
+use KiwiSuite\Contract\CommandBus\CommandInterface;
+use KiwiSuite\Contract\CommandBus\DispatchInterface;
+use KiwiSuite\Contract\CommandBus\HandlerInterface;
+use KiwiSuite\Contract\CommandBus\ResultInterface;
+use KiwiSuite\Validation\Validator;
+
+final class ValidationHandler implements HandlerInterface
+{
+    /**
+     * @var Validator
+     */
+    private $validator;
+
+    /**
+     * ValidationHandler constructor.
+     * @param Validator $validator
+     */
+    public function __construct(Validator $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    /**
+     * @param CommandInterface $command
+     * @param DispatchInterface $dispatcher
+     * @throws \Exception
+     * @return ResultInterface
+     */
+    public function handle(CommandInterface $command, DispatchInterface $dispatcher): ResultInterface
+    {
+        if (!$this->validator->supports($command)) {
+            return $dispatcher->dispatch($command);
+        }
+
+        $validationResult = $this->validator->validate($command);
+        if ($validationResult->isSuccessful()) {
+            return $dispatcher->dispatch($command);
+        }
+
+        return new Result(ResultInterface::STATUS_ERROR, $command, $validationResult->all());
+    }
+}
